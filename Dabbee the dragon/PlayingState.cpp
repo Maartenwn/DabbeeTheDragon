407c3e6cd1ea59accda473c42056a0c7045bd7b1
Unknown <ricky-1999@live.nl>
2018-05-28 16:08:54 +0200

    Made cool rotating hands

--- Dabbee the dragon/PlayingState.cpp
+++ Dabbee the dragon/PlayingState.cpp
@@ -62,7 +62,7 @@ void PlayingState::resize(int w, int h) {
 
 void PlayingState::drawHUD()
 {
-	float gsize = 10;
+	float gsize = 50;
 
 	glMatrixMode(GL_PROJECTION);
 	glPushMatrix();
@@ -72,33 +72,50 @@ void PlayingState::drawHUD()
 	glLoadIdentity();
 	glDisable(GL_CULL_FACE);
 
+
 	glClear(GL_DEPTH_BUFFER_BIT);
+	glColor4f(1.0f, 1.0f, 1.0f,0.0f);
+	glEnable(GL_TEXTURE_2D);
+	glDisable(GL_LIGHTING);
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	lefthand->bind();
 
-	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glColor4f(1.0, 1.0, 1.0, 0.5);//Replace this alpha for transparency
+
+	glPushMatrix();
+	glTranslatef(lhp.x, lhp.y, 0);
+	float rotationL = atan2(lhp.y - motionO.y, lhp.x - motionO.x) * 180.0f / 3.1415f;
+	glRotatef(rotationL + 90, 0, 0, 1);
 	glBegin(GL_QUADS);
-
 	if (leftHandPoint.x != -1)
 		lhp = leftHandPoint;
+	glTexCoord2f(1, 1);		glVertex2f( - gsize,- gsize);
+	glTexCoord2f(0, 1);		glVertex2f( gsize,  - gsize);
+	glTexCoord2f(0, 0);		glVertex2f( gsize,    gsize);
+	glTexCoord2f(1, 0);		glVertex2f( - gsize,  gsize);
+	glEnd();
+	glPopMatrix();
 
-	glVertex2f(lhp.x - gsize, lhp.y - gsize);
-	glVertex2f(lhp.x + gsize, lhp.y - gsize);
-	glVertex2f(lhp.x + gsize, lhp.y + gsize);
-	glVertex2f(lhp.x - gsize, lhp.y + gsize);
-
-	glVertex2f(20, 40);
-	glVertex2f(40,40);
-	glVertex2f(40,80);
-	glVertex2f(20,80);
-
+	righthand->bind();
+	glPushMatrix();
+	glTranslatef(rhp.x, rhp.y, 0);
+	float rotationR = atan2(rhp.y - motionO.y, rhp.x - motionO.x) * 180.0f / 3.1415f;
+	glRotatef(rotationR -270, 0, 0, 1);
+	glBegin(GL_QUADS);
 	if (rightHandPoint.x != -1)
 		rhp = rightHandPoint;
 
-	glVertex2f(rhp.x - gsize, rhp.y - gsize);
-	glVertex2f(rhp.x + gsize, rhp.y - gsize);
-	glVertex2f(rhp.x + gsize, rhp.y + gsize);
-	glVertex2f(rhp.x - gsize, rhp.y + gsize);
-
+	glTexCoord2f(1, 1);		glVertex2f(-gsize, -gsize);
+	glTexCoord2f(0, 1);		glVertex2f(gsize, -gsize);
+	glTexCoord2f(0, 0);		glVertex2f(gsize, gsize);
+	glTexCoord2f(1, 0);		glVertex2f(-gsize, gsize);
 	glEnd();
+	glPopMatrix();
+
+	glDisable(GL_BLEND);
+	glEnable(GL_LIGHTING);
 	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
 	glPushMatrix();
@@ -156,6 +173,10 @@ void PlayingState::update(float deltaTime)
 
 void PlayingState::init()
 {
+	motionO = Vec2f(width / 2, height / 2);
+	lefthand = new Texture("mickey_mouse_PNG39R.png");
+	righthand = new Texture("mickey_mouse_PNG39L.png");
+
 	hasFlapped = false;
 	hasCollided = false;
 	cameraOffset = { 0,0,0 };
