9f87ac7ac612caa3eba85c1c50955e57b20ade11
Unknown <ricky-1999@live.nl>
2018-05-29 11:36:26 +0200

    splitted handsCode

---
+++ Dabbee the dragon/MagicHands.h
@@ -0,0 +1,4 @@
+#pragma once
+void hand_draw(void);
+void hand_update(void);
+void hand_init(void);
--- Dabbee the dragon/PlayingState.cpp
+++ Dabbee the dragon/PlayingState.cpp
@@ -62,7 +62,6 @@ void PlayingState::resize(int w, int h) {
 
 void PlayingState::drawHUD()
 {
-	float gsize = 50;
 
 	glMatrixMode(GL_PROJECTION);
 	glPushMatrix();
@@ -75,48 +74,7 @@ void PlayingState::drawHUD()
 
 	glClear(GL_DEPTH_BUFFER_BIT);
 	glColor4f(1.0f, 1.0f, 1.0f,0.0f);
-	glEnable(GL_TEXTURE_2D);
-	glDisable(GL_LIGHTING);
-	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-	lefthand->bind();
-
-	glEnable(GL_BLEND);
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-	glColor4f(1.0, 1.0, 1.0, 0.5);//Replace this alpha for transparency
-
-	glPushMatrix();
-	glTranslatef(lhp.x, lhp.y, 0);
-	float rotationL = atan2(lhp.y - motionO.y, lhp.x - motionO.x) * 180.0f / 3.1415f;
-	glRotatef(rotationL + 90, 0, 0, 1);
-	glBegin(GL_QUADS);
-	if (leftHandPoint.x != -1)
-		lhp = leftHandPoint;
-	glTexCoord2f(1, 1);		glVertex2f( - gsize,- gsize);
-	glTexCoord2f(0, 1);		glVertex2f( gsize,  - gsize);
-	glTexCoord2f(0, 0);		glVertex2f( gsize,    gsize);
-	glTexCoord2f(1, 0);		glVertex2f( - gsize,  gsize);
-	glEnd();
-	glPopMatrix();
-
-	righthand->bind();
-	glPushMatrix();
-	glTranslatef(rhp.x, rhp.y, 0);
-	float rotationR = atan2(rhp.y - motionO.y, rhp.x - motionO.x) * 180.0f / 3.1415f;
-	glRotatef(rotationR -270, 0, 0, 1);
-	glBegin(GL_QUADS);
-	if (rightHandPoint.x != -1)
-		rhp = rightHandPoint;
-
-	glTexCoord2f(1, 1);		glVertex2f(-gsize, -gsize);
-	glTexCoord2f(0, 1);		glVertex2f(gsize, -gsize);
-	glTexCoord2f(0, 0);		glVertex2f(gsize, gsize);
-	glTexCoord2f(1, 0);		glVertex2f(-gsize, gsize);
-	glEnd();
-	glPopMatrix();
-
-	glDisable(GL_BLEND);
-	glEnable(GL_LIGHTING);
-	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	hand_draw();
 
 	glPushMatrix();
 	glTranslatef(30, 40 ,0);	//translate and scale
@@ -161,6 +119,7 @@ bool hasCollided;
 
 void PlayingState::update(float deltaTime)
 {
+	hand_update();
 	if (flapspeed != 0.0 || keys[' ']) hasFlapped = true;
 	if (hasFlapped) {
 		total_score += playingGame->update(deltaTime);
@@ -173,10 +132,7 @@ void PlayingState::update(float deltaTime)
 
 void PlayingState::init()
 {
-	motionO = Vec2f(width / 2, height / 2);
-	lefthand = new Texture("mickey_mouse_PNG39R.png");
-	righthand = new Texture("mickey_mouse_PNG39L.png");
-
+	hand_init();
 	hasFlapped = false;
 	hasCollided = false;
 	cameraOffset = { 0,0,0 };
