0d99643c8c64a83951a4d122bfaf5643be1c74d2
Lois Gussenhoven <lois_gussenhoven@hotmail.nl>
2018-06-04 17:45:56 +0200

    Added Font

--- Dabbee the dragon/PlayingState.cpp
+++ Dabbee the dragon/PlayingState.cpp
@@ -21,17 +21,44 @@ extern cv::Point leftHandPoint, rightHandPoint;
 bool hasFlapped;
 float total_score;
 
-
 extern int width;
 extern int height;
 
+int imgWidth, imgHeight;
 
+GLuint list_base;
+int bpp;
 
+static Texture* font;
 
 PlayingState::PlayingState(GameStateManager* manager)
 {
 	this->playingGame = new Game(manager);
 	this->manager = manager;
+	font = new Texture("ExportedFont1.png");
+
+	list_base = glGenLists(256);
+	glEnable(GL_TEXTURE_2D);
+	font->bind();
+	for (int i = 0; i < 256; i++) {
+		float cx = (float)(15 - i % 16) / 16.0f;
+		float cy = (float)(i / 16) / 16.0f;
+
+		float size = 1 / 16.0f;
+
+		glNewList(list_base + 255 - i, GL_COMPILE);
+
+		glBegin(GL_QUADS);
+
+		glTexCoord2f(cx, cy);					glVertex2d(0, 1);	
+		glTexCoord2f(cx, cy + size);			glVertex2i(0, 0);
+		glTexCoord2f(cx + size, cy + size);		glVertex2i(1, 0);
+		glTexCoord2f(cx + size, cy);			glVertex2i(1, 1);
+
+		glEnd();
+		glTranslated(1, 0, 0);
+		glEndList();
+	}
 }
 
 
@@ -40,6 +67,36 @@ PlayingState::~PlayingState()
 	total_score = 0;
 	delete playingGame;
 }
+
+void PlayingState::drawScore()
+{
+	glMatrixMode(GL_PROJECTION);
+	glPushMatrix();
+	glLoadIdentity();
+	glOrtho(0.0, width, height, 0.0, -1.0, 1.0);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glDisable(GL_CULL_FACE);
+
+	glClear(GL_DEPTH_BUFFER_BIT);
+
+
+	glDisable(GL_LIGHTING);
+	glDisable(GL_DEPTH_TEST);
+
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glColor3f(1.0f, 1.0f, 1.0f);
+
+	glPushMatrix();
+	glScalef(50, 50, 50);
+	font->bind();
+	glListBase(list_base);
+	string score = "Score: ";
+	score += to_string((int) total_score);
+	glCallLists(score.length(), GL_UNSIGNED_BYTE, score.c_str());
+	glPopMatrix();
+} 
 
 void PlayingState::resize(int w, int h) {
 
@@ -54,7 +111,7 @@ void PlayingState::drawHUD()
 	glLoadIdentity();
 	glOrtho(0.0, width, height, 0.0, -1.0, 10.0);
 	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
+	glLoadIdentity(); 
 	glDisable(GL_CULL_FACE);
 
 
@@ -87,7 +144,6 @@ void PlayingState::drawHUD()
     draw_debug_display(debugon);
 	
 	glPopMatrix();
-
 	glMatrixMode(GL_PROJECTION);
 	glPopMatrix();
 	glMatrixMode(GL_MODELVIEW);
@@ -97,6 +153,8 @@ void PlayingState::draw()
 {
 	playingGame->draw();
 	drawHUD();
+	drawScore();
+
 }
 
 extern float flapspeed;
